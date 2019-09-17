dde2b2bf5b46eaad179b5c92faa4bb74da53e815
Kristian Heruc <kristian@heruc.com>
2019-09-17 18:37:23 +0200

    Apply animations to post index

--- components/post-list.js
+++ components/post-list.js
@@ -1,20 +1,40 @@
 import Link from 'next/link';
+import { motion } from 'framer-motion';
 import PostInfo from './post-info';
 
+const postVariants = {
+  initial: { scale: 0.96, y: 30, opacity: 0 },
+  enter: { scale: 1, y: 0, opacity: 1, transition: { duration: 0.5, ease: [0.48, 0.15, 0.25, 0.96] } },
+  exit: {
+    scale: 0.6,
+    y: 100,
+    opacity: 0,
+    transition: { duration: 0.2, ease: [0.48, 0.15, 0.25, 0.96] }
+  }
+};
+
 const PostList = ({ posts }) => (
-  <div className="container">
+  <motion.div
+    initial="initial"
+    animate="enter"
+    exit="exit"
+    variants={{ exit: { transition: { staggerChildren: 0.1 } } }}
+  >
     <div className="posts">
       {posts.map(post => {
         return (
           <div key={post.id} className="post">
-            <Link href="/posts/[post]" as={`/posts/${post.id}`}>
-              <a>
-                <img src={`/static/images/${post.id}.jpg`} />
-
-                <div>{post.title}</div>
-              </a>
-            </Link>
-            <PostInfo post={post} />
+            <motion.div variants={postVariants}>
+              <Link href="/posts/[post]" as={`/posts/${post.id}`}>
+                <a>
+                  <motion.div whileHover="hover" variants={{ hover: { scale: 0.96 } }}>
+                    <img src={`/static/images/${post.id}.jpg`} />
+                  </motion.div>
+                  <div>{post.title}</div>
+                </a>
+              </Link>
+              <PostInfo post={post} />
+            </motion.div>
           </div>
         );
       })}
@@ -37,7 +57,7 @@ const PostList = ({ posts }) => (
         }
       `}</style>
     </div>
-  </div>
+  </motion.div>
 );
 
 export default PostList;
