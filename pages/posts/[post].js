c0376dfe789dfb496c46db63a7ec3b0bf56e0d5e
Kristian Heruc <kristian@heruc.com>
2019-09-17 18:37:51 +0200

    Apply animations to single post

--- pages/posts/[post].js
+++ pages/posts/[post].js
@@ -1,17 +1,67 @@
 import Link from 'next/link';
 import posts from '../../data/posts';
+import { motion } from 'framer-motion';
 import PostInfo from '../../components/post-info';
+
+let easing = [0.175, 0.85, 0.42, 0.96];
+
+const imageVariants = {
+  exit: { y: 150, opacity: 0, transition: { duration: 0.5, ease: easing } },
+  enter: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+      ease: easing
+    }
+  }
+};
+
+const textVariants = {
+  exit: { y: 100, opacity: 0, transition: { duration: 0.5, ease: easing } },
+  enter: {
+    y: 0,
+    opacity: 1,
+    transition: { delay: 0.1, duration: 0.5, ease: easing }
+  }
+};
+
+const backVariants = {
+  exit: {
+    x: 100,
+    opacity: 0,
+    transition: {
+      duration: 0.5,
+      ease: easing
+    }
+  },
+  enter: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      delay: 0.5,
+      duration: 0.5,
+      ease: easing
+    }
+  }
+};
 
 const Post = ({ post }) => (
   <div className="container post">
-    <img src={`/static/images/${post.id}.jpg`} />
+    <motion.div initial="exit" animate="enter" exit="exit">
+      <motion.img variants={imageVariants} src={`/static/images/${post.id}.jpg`} />
 
-    <PostInfo post={post} />
-    <p>{post.text}</p>
+      <motion.div variants={textVariants}>
+        <PostInfo post={post} />
+        <p>{post.text}</p>
+      </motion.div>
 
-    <Link href="/">
-      <a>Back to list</a>
-    </Link>
+      <motion.div variants={backVariants}>
+        <Link href="/">
+          <a>Back to list</a>
+        </Link>
+      </motion.div>
+    </motion.div>
 
     <style jsx>{`
       .post {
