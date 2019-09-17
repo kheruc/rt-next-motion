753a5e39fc883184de7277274e156af484520ec9
Kristian Heruc <kristian@heruc.com>
2019-09-17 18:36:41 +0200

    Add AnimatePresence to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import App from 'next/app';
+import { AnimatePresence } from 'framer-motion';
 
 import Layout from '../components/layout';
 
 class MyApp extends App {
   render() {
-    const { Component, pageProps } = this.props;
+    const { Component, pageProps, router } = this.props;
 
     return (
       <Layout>
-        <Component {...pageProps} />
+        <AnimatePresence exitBeforeEnter>
+          <Component {...pageProps} key={router.route} />
+        </AnimatePresence>
       </Layout>
     );
   }
